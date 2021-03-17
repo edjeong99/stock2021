5ce161712b1654eaf90d45032c71877bee01333a
Edward J <edjeong99@gmail.com>
2021-03-17 17:33:31 -0500

    edit util

---
+++ src/util/Reference.js
@@ -0,0 +1,8 @@
+QUOTE_API_URL = 'https://cloud.iexapis.com/v1/stock/';
+QUOTE_API_URL2 = '/quote';
+API_TOKEN = '?token=pk_9d1575de6ba8426b9a036edc8cd74274';
+
+STOCK_SYMBOL_LIST = ['AMZN', 'TRIP', 'AAPL', 'TSLA', 'WMT'];
+
+SEARCH_URL1 = 'https://finnhub.io/api/v1/search?q=';
+SEARCH_URL2 = '&token=c17tckv48v6reqlb2f90';
